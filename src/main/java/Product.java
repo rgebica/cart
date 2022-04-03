62914d79529e6a8c99d75b737b5d3e0695dc83b5
rgebica <rgebica@gmail.com>
2022-04-03 13:28:30 +0200

    added tests for checking adding product to cart method

---
+++ src/main/java/Product.java
@@ -0,0 +1,27 @@
+public class Product {
+    private final String productName;
+    private final int price;
+    private int quantity;
+
+    public Product(String productName, int price, int quantity) {
+        this.productName = productName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
--- src/test/java/CartTest.groovy
+++ src/test/java/CartTest.groovy
@@ -2,4 +2,34 @@ import spock.lang.Specification
 
 class CartTest extends Specification {
 
+    def "shouldAddProductToCart"() {
+        given:
+        def cart = new Cart()
+
+        when:
+        boolean result = cart.addProducts("Apple", 123, 1)
+
+        then:
+        result
+    }
+
+    def "shouldNotAddProductToCart"() {
+        given:
+        def cart = new Cart()
+
+        when:
+        boolean result = cart.addProducts(productName, price, quantity)
+
+        then:
+        result == expected
+
+        where:
+        productName || price || quantity || expected
+        "Apple"     || 123   || -1       || false
+        "Milka"     || -123  || 0        || false
+        "Orange"    || null  || null     || false
+        ""          || -123  || null     || false
+        null        || 0     || null     || false
+    }
+
 }
